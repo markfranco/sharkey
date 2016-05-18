eeacb0c3a3044057a6348e76f2446a97c4f20b90
Mark Franco <mark.f@prospa.com>
2016-05-19 09:27:04 +1000

    brett fixed this

--- ios/snowflakeTests/snowflakeTests.m
+++ ios/snowflakeTests/snowflakeTests.m
@@ -14,7 +14,7 @@
 #import "RCTRootView.h"
 
 #define TIMEOUT_SECONDS 240
-#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+#define TEXT_TO_LOOK_FOR @"Snowflake"
 
 @interface snowflakeTests : XCTestCase
 
